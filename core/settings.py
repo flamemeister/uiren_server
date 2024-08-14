9e60d664c7a1b373ef93e011070d41c5efc29eff
Aldiyar <120413003+flamemeister@users.noreply.github.com>
2024-08-14 17:29:41 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ import os
 
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
